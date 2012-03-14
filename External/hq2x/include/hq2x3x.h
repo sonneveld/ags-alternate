d9c0505dcc830aa4d725968f2a21ce9bed9e1009
Nick Sonneveld <nick@sonneveld.name>
2012-03-14 11:59:29 +1100

    move around libcda and hq2x3x

---
+++ External/hq2x/include/hq2x3x.h
@@ -0,0 +1,3 @@
+extern void InitLUTs();
+extern void hq2x_32( unsigned char * pIn, unsigned char * pOut, int Xres, int Yres, int BpL );
+extern void hq3x_32( unsigned char * pIn, unsigned char * pOut, int Xres, int Yres, int BpL );
