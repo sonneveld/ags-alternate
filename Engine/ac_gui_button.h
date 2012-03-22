3dba873f9988ea4c101aac6436cefcaed3a73baa
Nick Sonneveld <nick@sonneveld.name>
2012-03-22 14:01:13 +1100

    pick out gui button funcs from ac.cpp

---
+++ Engine/ac_gui_button.h
@@ -0,0 +1,9 @@
+#ifndef _AC_GUI_BUTTON_H_HEADER
+#define _AC_GUI_BUTTON_H_HEADER
+
+extern int UpdateAnimatingButton(int bu);
+extern void StopButtonAnimation(int idxn);
+
+extern void register_button_script_functions();
+
+#endif
